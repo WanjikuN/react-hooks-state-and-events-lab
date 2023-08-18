bc8785e78d1ef73b304f2e57e64caa0e1b315dea
WanjikuN <cikunjoroge4@gmail.com>
2023-08-18 07:06:17 +0300

    Update item on cart

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [cart, setCart] = useState(false)
+  const classMode = cart?"in-cart":"";
+  function handleClick(){
+    setCart(cart => !cart);
+  }
   return (
-    <li className="">
+    <li className={classMode}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick ={handleClick} className="add">{cart?"Remove From Cart":"Add to Cart"}</button>
     </li>
   );
 }
